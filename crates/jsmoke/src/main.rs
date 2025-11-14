6154b4f25cf723d6fea6000db27480a11e640739
Nasccped <pedro.main.tec@gmail.com>
2025-11-14 20:19:06 -0300

    chore: change bin dest. + include cli module

--- crates/jsmoke/src/main.rs
+++ crates/jsmoke/src/main.rs
@@ -1,4 +1,15 @@
+/// Exit the current program with the provided code value:
+/// - ok/warning as `0`
+/// - internal error as `N` when `N` isn't equals than `0`
+///
+/// No effect when exiting with `0` (means the same as reaching the
+/// end of the code).
+fn exit_with_code(code: i32) -> ! {
+    std::process::exit(code);
+}
+
 fn main() {
-    let turn_green = |text: &str| format!("\x1b[92m{text}\x1b[0m");
-    println!("Hello from {}!!!", turn_green("jsmoke crates"));
+    // unused var
+    let _app = jsmoke_cli::parse();
+    exit_with_code(0);
 }
