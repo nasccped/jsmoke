4cb7ff739a63303990870ad0c4371afd25b253f9
Nasccped <pedro.main.tec@gmail.com>
2025-11-15 09:41:09 -0300

    chore(run/test): add version comment + behave on verbose flag

--- crates/jsmoke/src/main.rs
+++ crates/jsmoke/src/main.rs
@@ -1,11 +1,23 @@
 #![doc = include_str!("../README.md")]
 
+use jsmoke_cli::prelude::app::JsmkApp;
 use jsmoke_utils::exit;
 
+/// # Override the program version
+///
+/// Since [`JsmkApp`] is generated within the [`jsmoke_cli`] lib,
+/// it'll take the lib version when trying to parse the object.
+/// Instead, we should call the [`jsmoke_cli::parse`] function and
+/// provide the current crate version (same as the binary final
+/// binary).
 const PROGRAM_VERSION: &str = env!("CARGO_PKG_VERSION");
 
 fn main() {
-    // unused var
-    let _app = jsmoke_cli::parse(PROGRAM_VERSION);
-    exit::exit_with_code(0);
+    let app: JsmkApp = jsmoke_cli::parse(PROGRAM_VERSION);
+    if app.verbose {
+        println!("Exiting error with verbose!");
+        exit::exit_with_code(1);
+    } else {
+        println!("No verbose? No error!");
+    }
 }
