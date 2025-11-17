95ec6b288369b18fb539bf45ab9728ff86dec635
Nasccped <pedro.main.tec@gmail.com>
2025-11-17 20:41:43 -0300

    chore(big update): code visual + add PrintError proc-macro + const use

--- crates/jsmoke_utils/src/visuals/tags.rs
+++ crates/jsmoke_utils/src/visuals/tags.rs
@@ -4,18 +4,19 @@
 //! its features.
 
 use super::color_highlights::ColorHighlights;
-use crate::verbose::Verbose;
+use crate::{
+    printing::error_print::ErrorPrint,
+    strings::repr::{STRING_SLICE, TAG_KIND, TAGS_MODULE},
+    verbose::Verbose,
+};
 use colored::Colorize;
+use jsmoke_macros::ErrorPrint;
 use thiserror::Error;
 
-/// Represents [`TagKind`] as string.
-const TAGKIND_REPR: &str = "`TagKind`";
-
-/// Represents [`self`] module as string.
-const TAGSMOD_REPR: &str = "`jsmoke_utils::visuals::tags`";
-
-/// Represents [`str`] slice as string.
-const STRING_SLICE_REPR: &str = "`&str`";
+const ERROR: &str = "error";
+const WARNING: &str = "warn";
+const NOTE: &str = "note";
+const OK: &str = "ok";
 
 /// Tag kind when reporting messages to user.
 #[derive(Debug, PartialEq)]
@@ -28,6 +29,19 @@ pub enum TagKind {
     NoteKind,
     /// The job was done successfully!
     OkKind,
+}
+
+impl TagKind {
+    /// Converts the provided [`TagKind`] into a simple string (no
+    /// color/colon).
+    fn as_simple_str(&self) -> &'static str {
+        match self {
+            TagKind::ErrorKind => ERROR,
+            TagKind::WarningKind => WARNING,
+            TagKind::NoteKind => NOTE,
+            TagKind::OkKind => OK,
+        }
+    }
 }
 
 /// private available kinds (used to print within
@@ -43,12 +57,12 @@ impl std::fmt::Display for TagKind {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
-            "{}:",
+            "{}: ",
             match self {
-                Self::ErrorKind => "error".bright_red(),
-                Self::WarningKind => "warn".bright_yellow(),
-                Self::NoteKind => "note".bright_cyan(),
-                Self::OkKind => "ok".bright_green(),
+                Self::ErrorKind => ERROR.bright_red(),
+                Self::WarningKind => WARNING.bright_yellow(),
+                Self::NoteKind => NOTE.bright_cyan(),
+                Self::OkKind => OK.bright_green(),
             }
         )
     }
@@ -58,10 +72,10 @@ impl<'a> TryFrom<&'a str> for TagKind {
     type Error = UnparseableTagKind;
     fn try_from(value: &'a str) -> Result<Self, Self::Error> {
         match value.to_lowercase().trim() {
-            "error" => Ok(Self::ErrorKind),
-            "warn" => Ok(Self::WarningKind),
-            "note" => Ok(Self::NoteKind),
-            "ok" => Ok(Self::OkKind),
+            ERROR => Ok(Self::ErrorKind),
+            WARNING => Ok(Self::WarningKind),
+            NOTE => Ok(Self::NoteKind),
+            OK => Ok(Self::OkKind),
 
             // pass any other string (even empty) to `from` method
             // since they all are invalid
@@ -71,7 +85,7 @@ impl<'a> TryFrom<&'a str> for TagKind {
 }
 
 /// Error obj. when TagKind parse fails.
-#[derive(Error, Debug, PartialEq)]
+#[derive(Error, Debug, PartialEq, ErrorPrint)]
 pub enum UnparseableTagKind {
     /// The provide [`str`] isn't a valid TagKind.
     #[error("couldn't parse the provided kind value (`{0}`)")]
@@ -93,31 +107,31 @@ impl From<&str> for UnparseableTagKind {
 
 impl Verbose for UnparseableTagKind {
     fn get_verbose(&self) -> String {
-        let mut s = format!(
-            "This occurs when trying to generate a {} from an invalid {} value.\n",
-            TAGKIND_REPR.type_highlight(),
-            STRING_SLICE_REPR.type_highlight()
-        );
-        s.push_str(
-            format!(
-                "Current available {} values: \n",
-                TAGKIND_REPR.type_highlight()
-            )
-            .as_str(),
-        );
-        AVAILABLE_KINDS
+        let tk = TAG_KIND.type_highlight();
+        let str_slc = STRING_SLICE.type_highlight();
+        let available = AVAILABLE_KINDS
             .iter()
-            .for_each(|kind| s.push_str(format!("  - {}\n", kind).as_str()));
-        s.push_str(
-            format!(
-                "{} {} referes to {} module.",
-                TagKind::NoteKind,
-                TAGKIND_REPR.type_highlight(),
-                TAGSMOD_REPR.module_highlight()
-            )
-            .as_str(),
-        );
-        s
+            .map(|k| format!("  - {}", k.as_simple_str()))
+            .collect::<Vec<_>>()
+            .join("\n");
+        let cur_mod = TAGS_MODULE.module_highlight();
+
+        format!(
+            "\
+            This occurs when trying to generate a {} from an\n\
+            invalid {} value.\n\
+            \n\
+            Currently available {} values:\n\
+            {}\n\
+            \n\
+            {}refers to {} module.",
+            tk,
+            str_slc,
+            tk,
+            available,
+            TagKind::NoteKind,
+            cur_mod
+        )
     }
 }
 
@@ -127,6 +141,7 @@ mod tests {
 
     #[test]
     fn ok_parsing() {
+        // use literals to avoid accidental const changes.
         [
             ("error", TagKind::ErrorKind),
             ("warn", TagKind::WarningKind),
