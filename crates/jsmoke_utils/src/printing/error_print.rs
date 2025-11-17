76ee1ab1f20374b18556bea227032bfa8c355abc
Nasccped <pedro.main.tec@gmail.com>
2025-11-17 20:36:52 -0300

    fix: add missing `PrintError` trait

---
+++ crates/jsmoke_utils/src/printing/error_print.rs
@@ -0,0 +1,16 @@
+use crate::visuals::tags::TagKind;
+
+/// Tag used when printing an error.
+const ERROR_TAG: TagKind = TagKind::ErrorKind;
+
+/// Trait for pretty error printing.
+///
+/// This trait is expected to be applied to any type that also
+/// implements the [`std::error::Error`] and the
+/// [`std::fmt::Display`] traits (since both traits are auto impl if
+/// the given type have [`derive`] with [`thiserror::Error`]).
+pub trait ErrorPrint: std::error::Error {
+    fn print_error(&self) {
+        println!("{}{}", ERROR_TAG, self);
+    }
+}
---
+++ crates/jsmoke_utils/src/printing/mod.rs
@@ -0,0 +1,8 @@
+//! # Printing module
+//!
+//! Printing related stuff:
+//! - error pretty print
+//! - warning print
+//! - ...
+
+pub mod error_print;
