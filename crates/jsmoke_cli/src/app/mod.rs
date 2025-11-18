e669e25e55d0e183842307a68a77e41a291023da
Nasccped <pedro.main.tec@gmail.com>
2025-11-17 21:42:46 -0300

    chore: add subcommand field + verbose as global + mod. doc

--- crates/jsmoke_cli/src/app/mod.rs
+++ crates/jsmoke_cli/src/app/mod.rs
@@ -1,6 +1,14 @@
-mod style;
+//! # App module
+//!
+//! Provides the main CLI app implementing.
+//!
+//! All the implementing is done through the [`clap::Parser`]
+//! proc-macro.
 
+mod style;
+pub mod subcommands;
 use clap::Parser;
+use subcommands::JsmkSubcommand;
 
 /// The JSmoke cli app.
 #[derive(Parser)]
@@ -12,7 +20,11 @@ use clap::Parser;
     styles = style::APP_STYLE
 )]
 pub struct JsmkApp {
+    /// Operation to perform.
+    #[command(subcommand)]
+    pub subcommand: Option<JsmkSubcommand>,
+
     /// Enable verbose (detailed info printing) during process.
-    #[arg(long)]
+    #[arg(long, global = true)]
     pub verbose: bool,
 }
