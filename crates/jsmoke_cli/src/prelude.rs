994b3c442c7b253c0e95d2ad03152e3b18ad6d28
Nasccped <pedro.main.tec@gmail.com>
2025-11-17 21:43:34 -0300

    chore: add `prelude`'s accesses

--- crates/jsmoke_cli/src/prelude.rs
+++ crates/jsmoke_cli/src/prelude.rs
@@ -6,6 +6,14 @@ pub mod app {
     //! # App module
     //!
     //! Provides the [`JsmkApp`] type access.
-
     pub use crate::app::JsmkApp;
+}
+
+pub mod subcommands {
+    //! # Subcommands module
+    //!
+    //! Provides the [`subcommands`] mods/types access.
+    #[allow(unused_imports)]
+    use crate::app::subcommands;
+    pub use crate::app::subcommands::JsmkSubcommand;
 }
