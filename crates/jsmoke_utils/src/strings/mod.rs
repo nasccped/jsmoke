f9433d90ba0dd549a6425e795b0861a3270d861d
Nasccped <pedro.main.tec@gmail.com>
2026-01-09 14:56:58 -0300

    chore: empty utilities lib

--- crates/jsmoke_utils/src/strings/mod.rs
+++
@@ -1,9 +0,0 @@
-//! # String Utilities
-//!
-//! This mod provides no builtin utils (or abstraction to long coded
-//! features).
-//!
-//! Each submodule contains it's own trait/types/funcs. Consider
-//! reading each documentation.
-pub mod chunk_by_any;
-pub mod repr;
