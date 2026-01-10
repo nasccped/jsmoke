bada00675d4a7fd49385424bd1666ef863850424
Nasccped <pedro.main.tec@gmail.com>
2026-01-10 09:58:35 -0300

    temp: stop cargo clippy arguing

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,10 @@ mod cli;
 mod exit;
 
 use clap::Parser;
+use std::process::ExitCode;
 
 fn main() {
     let mut _app = cli::App::parse();
+    // stop clippy arguing.
+    exit::with_code(ExitCode::SUCCESS);
 }
