228707cb34a673b2667b8f2da135c59f5b55b76c
Nasccped <pedro.main.tec@gmail.com>
2026-01-20 18:01:37 -0300

    chore: add local printer type

--- src/utils/verbose.rs
+++ src/utils/verbose.rs
@@ -1,5 +1,7 @@
 /// Verbose trait. Allows to hold/show verbose data (used when `verbose` flag is enabled).
 pub trait Verbose {
+    /// An [`super::printer::Printer`] like type to allow smart printing (stdout/stderr).
+    type LocalPrinter<'a>;
     /// Prints the verbose data based on the `self` item reference.
-    fn print_verbose(&self);
+    fn print_verbose(&self, printer: Self::LocalPrinter<'_>);
 }
