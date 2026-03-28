323606568d59de1b033e0571d38d3614f26ea073
Nasccped <pedro.main.tec@gmail.com>
2026-03-28 11:42:44 -0300

    chore: flex code with `Into` trait

--- src/exit.rs
+++ src/exit.rs
@@ -1,8 +1,8 @@
 use std::process::{self, ExitCode};
 
 /// Exit's the current program with the provided exit code.
-pub fn with_code(code: ExitCode) -> ! {
-    match code {
+pub fn with_code<T: Into<ExitCode>>(code: T) -> ! {
+    match code.into() {
         ExitCode::SUCCESS => process::exit(0),
         _ => process::exit(1),
     }
