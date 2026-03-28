06006a54594ae74657df011bccbb1f7b3a346728
Nasccped <pedro.main.tec@gmail.com>
2026-03-28 11:54:40 -0300

    chore: start runtime module

---
+++ src/runtime/mod.rs
@@ -0,0 +1,40 @@
+use crate::cli::App;
+use std::process::ExitCode;
+
+/// Type that mimics the a [`Result`] enum since I can't implement `Into<ExitCode>` for foreign
+/// types.
+pub enum Output {
+    /// Ok variant.
+    Ok(OkVariant),
+    /// Error variant.
+    Err(ErrVariant),
+}
+
+/// Temp type for Ok shadowing...
+pub struct OkVariant;
+/// Temp type for Err shadowing...
+pub struct ErrVariant;
+
+#[allow(clippy::from_over_into)]
+impl Into<ExitCode> for Output {
+    fn into(self) -> ExitCode {
+        match self {
+            Output::Ok(_) => ExitCode::SUCCESS,
+            _ => ExitCode::FAILURE,
+        }
+    }
+}
+
+/// Run the jsmoke program based on the [`App`] inner fields.
+pub fn run(app: App) -> Output {
+    if app.verbose {
+        println!("verbose is enabled");
+    }
+    if app.force {
+        println!("forcing action...");
+        Output::Ok(OkVariant)
+    } else {
+        eprintln!("no action being forced! Returning an err");
+        Output::Err(ErrVariant)
+    }
+}
