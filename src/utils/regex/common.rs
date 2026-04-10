dacb82a90b0cc95dec7456e9fb8bf62acd47fd20
Nasccped <pedro.main.tec@gmail.com>
2026-04-09 21:26:21 -0300

    chore: add starts/ends/with regex functions

--- src/utils/regex/common.rs
+++ src/utils/regex/common.rs
@@ -9,6 +9,52 @@ pub static EMPTY_REGEX: LazyLock<Regex> = LazyLock::new(|| Regex::new(r#"^$"#).s
 pub static WHITESPACE_REGEX: LazyLock<Regex> =
     LazyLock::new(|| Regex::new(r#"^[\s]+$"#).surely_unwrap());
 
-/// Regex for ascii char detection.
-pub static ASCII_CHAR_REGEX: LazyLock<Regex> =
+/// Regex for ascii alphabetic detection. Works both for lowercase and uppercase.
+pub static ASCII_ALPHABETIC_REGEX: LazyLock<Regex> =
     LazyLock::new(|| Regex::new(r#"[a-zA-Z]"#).surely_unwrap());
+
+/// Function to `startswith` detection. Note that this variable returns a function that results
+/// into a [`Regex`] (unwrap at initialization), so, it can [`panic`] if invalid regex syntax is
+/// used.
+///
+/// Regex's begin symbol (`^`) isn't required since it's pushed at initialization.
+pub static STARTS_WITH_REGEX: LazyLock<fn(&str) -> Regex> = LazyLock::new(|| starts_with_regex);
+
+/// Function to `endswith` detection. Note that this variable returns a function that results
+/// into a [`Regex`] (unwrap at initialization), so, it can [`panic`] if invalid regex syntax is
+/// used.
+///
+/// Regex's begin symbol (`^`) isn't required since it's pushed at initialization.
+pub static ENDS_WITH_REGEX: LazyLock<fn(&str) -> Regex> = LazyLock::new(|| ends_with_regex);
+
+/// Creates a new [`Regex`] with the provided regex pattern. Can [`panic`] since the
+/// [`SurelyUnwrap::surely_unwrap`] is used before returning.
+pub static NEW_REGEX_WITH: LazyLock<fn(&str) -> Regex> = LazyLock::new(|| new_regex_with);
+
+/// Private function wrapper for [`STARTS_WITH_REGEX`] lock.
+///
+/// It takes a str slice as pattern and produces a [`Regex`] over it. Can [`panic`] since it uses
+/// [`SurelyUnwrap::surely_unwrap`] before returning.
+fn starts_with_regex(pattern: &str) -> Regex {
+    let mut s = String::from("^");
+    s.push_str(pattern);
+    Regex::new(s.as_str()).surely_unwrap()
+}
+
+/// Private function wrapper for [`ENDS_WITH_REGEX`] lock.
+///
+/// It takes a str slice as pattern and produces a [`Regex`] over it. Can [`panic`] since it uses
+/// [`SurelyUnwrap::surely_unwrap`] before returning.
+fn ends_with_regex(pattern: &str) -> Regex {
+    let mut s = String::from(pattern);
+    s.push('$');
+    Regex::new(s.as_str()).surely_unwrap()
+}
+
+/// Private function wrapper for [`NEW_REGEX_WITH`] lock.
+///
+/// It takes a str slice as pattern and produces a [`Regex`] over it. Can [`panic`] since it uses
+/// [`SurelyUnwrap::surely_unwrap`] before returning.
+fn new_regex_with(pattern: &str) -> Regex {
+    Regex::new(pattern).surely_unwrap()
+}
