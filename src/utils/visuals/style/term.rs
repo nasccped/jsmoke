5df6052d5ea8ebdd29eac25b109b2d301ac09be6
Nasccped <pedro.main.tec@gmail.com>
2026-04-25 12:24:58 -0300

    fix: impl for `&str` and `String` directly

--- src/utils/visuals/style/term.rs
+++ src/utils/visuals/style/term.rs
@@ -1,13 +1,19 @@
 use colored::{ColoredString, Colorize};
 
 /// Apply term styling to the item. Requires item to implements [`Colorize`] and [`Clone`] trait.
-pub trait TermStyle: Colorize + Clone {
+pub trait TermStyle {
     /// Apply styles form term like "there's some `constraints` over it..."
     fn term_style(&self) -> ColoredString;
 }
 
-impl<T: Colorize + Clone> TermStyle for T {
+impl TermStyle for String {
     fn term_style(&self) -> ColoredString {
-        self.clone().italic()
+        self.italic()
+    }
+}
+
+impl TermStyle for &str {
+    fn term_style(&self) -> ColoredString {
+        self.italic()
     }
 }
