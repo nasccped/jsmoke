a98631fde33ea4838d41dcfd59884103bf745544
Nasccped <pedro.main.tec@gmail.com>
2026-04-26 23:13:59 -0300

    chore: mod actions by func

--- src/runtime/mod.rs
+++ src/runtime/mod.rs
@@ -1,54 +1,112 @@
 mod common;
-mod operation;
-mod output;
+mod global_context;
+mod global_printer;
+mod no_subcommand_err;
+mod operations;
+mod runtime_output;
 
-use crate::{
-    cli::{App, subcommands::Subcommand},
-    runtime::{
-        operation::OperationTrait,
-        output::{OutputNotify, RuntimeOutput},
-    },
-    utils::{Verbose, visuals::style::CommandStyle},
-};
-pub use output::{IntoErr, IntoExitCode, IntoOk};
-use std::process::ExitCode;
-use thiserror::Error as ThisError;
+use crate::cli::{App, subcommands::Subcommand};
+use crate::runtime::runtime_output::{FailureConstraint, RuntimeOutputUtils};
+use crate::utils::notifiers::{NotifyFailure, NotifySuccess};
+use global_context::GlobalContext;
+use global_printer::GlobalPrinter;
+use no_subcommand_err::NoSubcommand;
+use operations::OperationTrait;
+use runtime_output::RuntimeOutput;
+use std::{cell::RefCell, ops::Deref, process::ExitCode, rc::Rc};
 
-/// Unit error when no subcommand is passed.
-#[derive(ThisError, Debug)]
-#[error("no subcommand given")]
-struct NoSubcommand;
-
-impl Verbose for NoSubcommand {
-    fn print_verbose(&self) {
-        eprintln!("Consider using {}", "jsmk --help".command_style());
-    }
-}
+/// Type alias for [`GlobalContext`] mutable cell.
+type Context = Rc<RefCell<GlobalContext>>;
 
 /// Run the jsmoke program based on the [`App`] inner fields.
 pub fn run(app: App) -> ExitCode {
-    let (subcommand, out, force, verbose): (Subcommand, RuntimeOutput, bool, bool);
-    (force, verbose) = (app.force, app.verbose);
-    match app.subcommand {
-        Some(s) => subcommand = s,
-        None => {
-            out = NoSubcommand.into_err();
-            out.output_notify();
-            if verbose {
-                out.print_verbose();
-            }
-            return out.into_exit_code();
+    let ctx = context_from_app(&app);
+    let out = pipe::get_piped_output(app.subcommand, ctx.clone());
+    notify_ouput(&out, ctx.clone());
+    out.get_exit_code()
+}
+
+/// Generates a [`Context`] item and set it up based on an [`App`] reference.
+fn context_from_app(app: &App) -> Context {
+    let mut gctx = GlobalContext::default();
+    gctx.with_force(app.force);
+    gctx.with_verbose(app.verbose);
+    Rc::new(RefCell::new(gctx))
+}
+
+/// Notify the provided [`RuntimeOutput`] based on [`Context`] inner fields. It automatically
+/// changes the [`GlobalPrinter`] stderr field to the designated [`Result`] variant. Also prints
+/// verbose if necessary.
+fn notify_ouput(out: &RuntimeOutput, ctx: Context) {
+    let mut ctx = ctx.borrow_mut();
+    let verb = ctx.is_verbose();
+    let mut printer = ctx.get_mut_printer();
+    match &out {
+        Ok(ok) => {
+            let success: &dyn NotifySuccess = ok.deref();
+            printer.print_success(success);
+        }
+        Err(e) => {
+            printer.set_stderr(true);
+            let failure: &dyn NotifyFailure = e.deref();
+            printer.print_failure(failure);
         }
     }
-    out = match subcommand {
-        Subcommand::New(x) => {
-            operation::NewOperation::try_from(x).and_then(|o| o.run(force, verbose))
-        }
-        other => unreachable!("this subcommand was called: {:?}", other),
+    if verb {
+        printer.print_verbose(out.as_verbose())
+    }
+}
+
+mod pipe {
+    //! # Pipe
+    //!
+    //! Stores the [`Subcommand`] to [`OperationTrait`] object pipe generating.
+    use super::{
+        Context, FailureConstraint, NoSubcommand, OperationTrait, RuntimeOutput, Subcommand,
+        operations::New as NewOperation,
     };
-    out.output_notify();
-    if verbose {
-        out.print_verbose();
+    use crate::cli::subcommands::New;
+
+    /// Type alias for [`Result`] over a subcommand and a dynamic [`FailureConstraint`].
+    ///
+    /// That's better than just [`Result<Subcommand, NoSubcommand>`] since report functions expects dyn
+    /// trait objects instead of concrete type.
+    type SubcommandOrFailure = Result<Subcommand, Box<dyn FailureConstraint>>;
+
+    /// Type alias for [`Result`] over operation and failure.
+    /// Used on the following context:
+    /// ```no_run
+    /// match OperationOrFailure::get_type() {
+    ///     Ok(oper) => {
+    ///         // run operation and get ouput...
+    ///     }
+    ///     Err(e) => {
+    ///         // handle operation gen. error
+    ///     }
+    /// }
+    /// ```
+    type OperationOrFailure = Result<Box<dyn OperationTrait>, Box<dyn FailureConstraint>>;
+
+    /// Runs a 'pipe' over provided inputs to generate the final [`RuntimeOutput`].
+    pub fn get_piped_output(subcmd: Option<Subcommand>, context: Context) -> RuntimeOutput {
+        let subcmd = subcommand_or_err(subcmd)?;
+        let oper = operation_from_subcommand(subcmd)?;
+        oper.run(context.clone())
     }
-    out.into_exit_code()
+
+    /// Generates a [`Result`] over an optional [`Subcommand`]:
+    /// - [`Ok`] if some
+    /// - [`Err`] ([`NoSubcommand`]) otherwise
+    fn subcommand_or_err(sub: Option<Subcommand>) -> SubcommandOrFailure {
+        sub.ok_or_else(|| Box::new(NoSubcommand) as Box<dyn FailureConstraint>)
+    }
+
+    /// Creates an [`OperationOrFailure`] over a [`Subcommand`].
+    fn operation_from_subcommand(subcommand: Subcommand) -> OperationOrFailure {
+        let trynew = |x: Box<New>| NewOperation::try_from(*x).map(|o| o.into_boxed());
+        match subcommand {
+            Subcommand::New(n) => trynew(n),
+            other => unreachable!("{:?} operation not implemented...", other),
+        }
+    }
 }
