658a33715e6175708bdd1fdb9b331ff60b6a59c7
Nasccped <pedro.main.tec@gmail.com>
2026-05-24 00:40:22 -0300

    refactor: remove unnecessary traits

--- src/utils/inner.rs
+++
@@ -1,6 +0,0 @@
-/// Trait to borrow the inner data (as reference) from complex types. A lot useful for tuple
-/// structs to avoid the `Struct(pub item)` thing.
-pub trait Inner<T: ?Sized> {
-    /// Extracts the item inner data as reference.
-    fn inner(&self) -> &T;
-}
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -6,14 +6,10 @@ pub mod regex;
 pub mod verbose;
 pub mod visuals;
 
-mod inner;
 mod input_fix;
 mod may_from;
 mod surely_unwrap;
-mod trim_and_box;
 
-pub use inner::Inner;
 pub use input_fix::InputFix;
 pub use may_from::MayFrom;
 pub use surely_unwrap::SurelyUnwrap;
-pub use trim_and_box::TrimAndBox;
