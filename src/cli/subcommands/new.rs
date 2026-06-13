5997cbe98f9894dea7eb1c4ecd4e7cf419e9549b
Nasccped <pedro.main.tec@gmail.com>
2026-06-13 09:56:02 -0300

    fix: use `Option<String>` (handle parsing at runtime)

--- src/cli/subcommands/new.rs
+++ src/cli/subcommands/new.rs
@@ -1,20 +1,18 @@
 //! # New subcommand
 //!
 //! Field definition for the new subcommand.
-use crate::shared_models::artifact_wrapper::ArtifactWrapper;
 use clap::Args;
-use std::path::PathBuf;
 
 /// Subcommand responsible for create new projects within a new directory.
 #[derive(Args, Debug)]
 pub struct New {
     /// The artifact of the project being created.
-    pub artifact: Option<ArtifactWrapper>,
+    pub artifact: Option<String>,
     /// Where to place the new project (same as `artifact` by default).
     #[arg(long, short = 'p')]
-    pub path: Option<PathBuf>,
+    pub path: Option<String>,
     /// Lock the project to the version regex.
-    #[arg(long = "lock", short = 'l', value_name = "(>= | =)VERSION")]
+    #[arg(long = "lock", short = 'l', value_name = "(>=|=)VERSION")]
     pub lock_version: Option<String>,
     /// The author(s) of the project.
     #[arg(long, value_name = "NAME1<EMAIL1>,N2...")]
