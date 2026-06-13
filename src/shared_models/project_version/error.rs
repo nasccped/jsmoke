f3645030f9bd82e45b66dc486f6cce3a2fa280bd
Nasccped <pedro.main.tec@gmail.com>
2026-06-12 21:11:21 -0300

    chore: remove `Constraint` variant + add `Invalid` and impl

--- src/shared_models/project_version/error.rs
+++ src/shared_models/project_version/error.rs
@@ -7,10 +7,24 @@ pub enum ProjectVersionError {
     /// When the provided string doesn't follow the expected version regex pattern.
     #[error("not recognizable version string pattern ({})", .0)]
     Pattern(String),
-    /// When the provided [`super::ProjectVersion`] kind constraint isn't valid.
-    #[error("invalid constraint being used at version string ({})", .0)]
-    Constraint(String),
     /// When the error happens at string parsing.
     #[error("couldn't parse the version string ({})", .0)]
     Parse(String),
+    /// When the error happens at `is_valid` function.
+    #[error("version string contains invalid fields ({})", .0)]
+    Invalid(String),
+}
+
+impl ProjectVersionError {
+    /// Changes the inner [`String`] to the `value`'s [`String`] and returns the `self` item. Note
+    /// that this function consumes `self`.
+    pub fn set_inner(mut self, value: impl ToString) -> Self {
+        let str_poiter = match &mut self {
+            Self::Pattern(inner) => inner,
+            Self::Parse(inner) => inner,
+            Self::Invalid(inner) => inner,
+        };
+        *str_poiter = value.to_string();
+        self
+    }
 }
