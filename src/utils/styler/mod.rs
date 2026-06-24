65265faea215efc2035c4592a7dcad521a7861f1
Nasccped <pedro.main.tec@gmail.com>
2026-06-24 18:48:50 -0300

    chore: add strong style

--- src/utils/styler/mod.rs
+++ src/utils/styler/mod.rs
@@ -2,6 +2,7 @@
 mod command_style;
 mod output;
 
+use colored::{ColoredString, Colorize};
 use command_style::CommandStyle;
 pub use output::StylerOutput;
 use std::ops::Deref;
@@ -12,6 +13,11 @@ pub trait Styler: Deref<Target = str> {
     fn command_style(&self) -> StylerOutput<String> {
         let result = CommandStyle::from(self.as_ref());
         StylerOutput::Item(result.to_string())
+    }
+
+    /// Gets the self item as strong style (bright white).
+    fn strong_style(&self) -> StylerOutput<ColoredString> {
+        StylerOutput::Item(self.bright_white().bold())
     }
 }
 
