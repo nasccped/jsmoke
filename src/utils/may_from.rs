f187c40dcda3b2be9e3343af3b86d909b3df2d53
Nasccped <pedro.main.tec@gmail.com>
2026-06-25 18:28:00 -0300

    remove: no longer used modules

--- src/utils/may_from.rs
+++
@@ -1,5 +0,0 @@
-/// Works similar to [`TryFrom`] trait, but returns [`Option`] over `T`, instead.
-pub trait MayFrom<T>: Sized {
-    /// Returns an optional value of `Self` based on `T` value.
-    fn may_from(value: T) -> Option<Self>;
-}
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,18 +1,12 @@
 //! # jsmoke utilities module
 //!
 //! A module that provides useful data structures along the runtime.
-mod input_fix;
-mod may_from;
 pub mod notifier;
 mod reserveds;
 mod strings;
 pub mod styler;
-mod surely_unwrap;
 mod verbose;
 
-pub use input_fix::InputFix;
-pub use may_from::MayFrom;
 pub use reserveds::Reserveds;
 pub use strings::StringUtils;
-pub use surely_unwrap::SurelyUnwrap;
 pub use verbose::Verbose;
