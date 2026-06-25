ef8cec5a28923a22b742e63682846ae38f6780a6
Nasccped <pedro.main.tec@gmail.com>
2026-06-25 18:21:06 -0300

    chore: add models to crate visib.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod cli;
+mod models;
 mod services;
 mod shared_models;
 mod utils;
@@ -9,7 +10,15 @@ use services::AppService;
 
 fn main() {
     let output = match App::try_parse() {
-        Ok(_) => todo!("impl app run"),
+        Ok(app_state) => match AppService::try_from(&app_state) {
+            Ok(_) => {
+                todo!("Parse success. Behavior still in progress...");
+            }
+            Err(err) => {
+                err.self_notify();
+                err.into()
+            }
+        },
         Err(e) => AppService::handle_parse_fail(AppParseFail::from(e)),
     };
     AppService::exit_with_code(output);
